23940668cc0a5d61abbb0c69d1230f8d2039551e
Evi Owhotemu <evicoach@gmail.com>
2022-04-15 08:22:36 +0100

    Added tests

---
+++ test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
@@ -0,0 +1,33 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:number_trivia/core/usecases/usecase.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+
+import 'get_concrete_number_trivia_test.mocks.dart';
+
+@GenerateMocks([NumberTriviaRepository])
+void main() {
+  late GetRandomNumberTrivia usecase;
+  late MockNumberTriviaRepository mockNumberTriviaRepository;
+  setUp(() async {
+    mockNumberTriviaRepository = MockNumberTriviaRepository();
+    usecase = GetRandomNumberTrivia(mockNumberTriviaRepository);
+  });
+
+  const tNumber = 1;
+  const tNumberTrivia = NumberTrivia(number: 1, text: 'test');
+
+  test('should get trivia from the repository', ()async{
+    // Given
+      when(mockNumberTriviaRepository.getRandomNumberTrivia()).thenAnswer((_)async => const Right(tNumberTrivia));
+    // when
+    final result = await usecase(NoParams());
+    // then
+    expect(result, const Right(tNumberTrivia));
+    verify(mockNumberTriviaRepository.getRandomNumberTrivia());
+  });
+}
